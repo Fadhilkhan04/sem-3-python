d52fbd04bfb9ed11123ef76f13f7d94a5cce193a
Fadhil <114307570+Fadhilkhan04@users.noreply.github.com>
2023-08-10 10:35:53 +0530

    Add files via upload

---
+++ python/pgm2.py
@@ -0,0 +1,3 @@
+age=int(input("enter your age :"))
+if 17<age<23:
+    print("give up on your dreams and die")
---
+++ python/pgm3.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age :\n"))
+
+if age >=18:
+    print("you are eligible to vote")
+
+else :
+    print("you are not eligible to vote")
