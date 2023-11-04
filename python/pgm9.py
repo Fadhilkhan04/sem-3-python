893daef21f8e47433ae4488636a6012282e46a9a
Fadhilkhan04 <114307570+Fadhilkhan04@users.noreply.github.com>
2023-11-04 18:01:31 +0530

    9,10

--- python/pgm9.py
+++ python/pgm9.py
@@ -1,1 +1,19 @@
 #affix zeroes
+def zeroes(a,b,n):
+    
+    for i in range(a,b+1):
+        num_str = str(i)
+        num_len = len(num_str)
+        affixzeroes = n - num_len
+
+        if affixzeroes > 0 :
+            num_str = '0'*affixzeroes + num_str
+        print(num_str,end = ',')
+            
+# main area
+
+a = int(input("Enter the start value(a):"))
+b = int(input("Enter the end value(b):"))
+n = int(input("Enter the desired length :"))
+
+zeroes(a,b,n)
