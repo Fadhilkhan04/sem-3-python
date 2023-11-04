052db4dbd6a27338249b96d072d8f07a68dedbd7
Fadhilkhan04 <114307570+Fadhilkhan04@users.noreply.github.com>
2023-11-04 21:53:53 +0530

    11,12

--- python/pgm11.py
+++ python/pgm11.py
@@ -1,1 +1,34 @@
-#digits to alphabets
+uppercase_start = 65
+uppercase_end = 90
+lowercase_start = 97
+lowercase_end = 122
+
+print (f"Uppercase English ASCII values start from '{chr(uppercase_start)}' and end at '{chr(uppercase_end)}'.")
+print (f"Lowercase English ASCII values start from '{chr(lowercase_start)}' and end at '{chr(lowercase_end)}'.")
+
+def convert_to_alphabet(input_values) : 
+
+    characters=[]
+    for value in input_values:
+        try:
+            num=int(value)
+
+            if uppercase_start <= num <= uppercase_end or lowercase_start <= num <= lowercase_end:
+                characters.append(chr(num))
+            else:
+                print (f"Warning: {num} is not a valid ASCII value for English alphabet, skipping.")
+                
+        except ValueError:
+            print(f"Warning: '{value}' is not valid ASCII value, skipping.")
+    return characters
+        
+try:
+    input_values=input("Enter ASCII values seperated by spaces:").split()
+    output=convert_to_alphabet(input_values)
+
+    if output:
+        print(f"The converted characters are: {output}")
+    else:
+        print("No valid input provided.")
+except ValueError:
+    print("Invalid input. Please enter valid ASCII values seperated by spaces.")
--- python/pgm12.py
+++ python/pgm12.py
@@ -1,1 +1,33 @@
-#divisible by 3,5 or 7
+def contains_only_357(number):
+
+    allowed_digits={'3','5','7'}
+    return all(digit in allowed_digits for digit in number)
+
+while True:
+
+    user_input = input("Enter a number containing only 3,5, or 7:")
+
+    if contains_only_357(user_input):
+        break
+
+    else:
+        print("Invalid input. Please enter a number containing only 3,5, or 7.")
+
+number=int(user_input)
+
+if number%3==0:
+
+    print("3 is a divisor. This is your lucky number!")
+
+elif number%5==0:
+
+    print("5 is a divisor. This is your lucky number!")
+
+elif number%7==0:
+
+    print("7 is a divisor. This is your lucky number!")
+
+else:
+  
+    print("This number is not divisble by 3,5, or 7.")
+    
