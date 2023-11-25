da92113b8788dac9fe13fb924a86c280c67885c0
Fadhilkhan04 <114307570+Fadhilkhan04@users.noreply.github.com>
2023-11-25 11:14:02 +0530

    matrix

--- python/exam/matrix.py
+++ python/exam/matrix.py
@@ -2,8 +2,8 @@
 
 r = int(input("enter the rows"))
 c = int(input("enter the columns"))
-m1 = [[0]*r]*c
-m2 = [[0]*r]*c
+m1 = [[0]*c]*r
+m2 = [[0]*c]*r
 
 print("enter matrix 1")
 for i in range(0,r):
@@ -17,11 +17,11 @@ for i in range(0,r):
     print("enter element",i,j)
     m2[i][j] = int(input())
 
-sum = [[0]*r]*c
+sum = [[0]*c]*r
 
 for i in range(0,r):
   for j in range(0,c):
     sum[i][j] = m1[i][j] + m2[i][j]
 
-
+print(sum)
     
