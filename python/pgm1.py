9af02142122d5d40b290e832e2685b5e17d1d16e
Fadhilkhan04 <114307570+Fadhilkhan04@users.noreply.github.com>
2023-12-25 22:37:49 +0530

    change

--- python/pgm1.py
+++ python/pgm1.py
@@ -1,10 +1,12 @@
 
 def fact(n):
-    fact = 1
-    for i in range (1,n+1):
-        fact=fact*i
+    #fact = 1
+    #for i in range (1,n+1):
+     #   fact=fact*i
+    if n == 1 or n==0:
+        return 1
         
-    return fact
+    return n*fact(n-1)
 
 # main area
 n = int(input("enter the number to find factorial of :"))
